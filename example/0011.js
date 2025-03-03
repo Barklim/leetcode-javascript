0584e1815182814478a1f7ce89c418e3fccc2138
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-04 02:07:26 +0300

    Create 0011.js

---
+++ example/0011.js
@@ -0,0 +1,27 @@
+/**
+ * 11. Container With Most Water
+ * https://leetcode.com/problems/container-with-most-water/
+ * Difficulty: Medium
+ *
+ * Given n non-negative integers `a1, a2, ..., an ,` where each represents a point at
+ * coordinate `(i, ai)`. `n` vertical lines are drawn such that the two endpoints of
+ * the line `i` is at `(i, ai)` and `(i, 0)`. Find two lines, which, together with the
+ * x-axis forms a container, such that the container contains the most water.
+ *
+ * Notice that you may not slant the container.
+ */
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+
+};
+  
+
+const example1 = maxArea([1,8,6,2,5,4,8,3,7]); // 49
+const example2 = maxArea([1,1]); // 1
+
+console.log(example1);
+console.log(example2);
