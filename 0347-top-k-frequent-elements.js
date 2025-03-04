f5a7c9fa5cfcfaab84bbcbce7d70ced1603203ce
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-04 16:18:31 +0300

    Create 0347.js

--- 0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements.js
@@ -22,3 +22,33 @@ var topKFrequent = function(nums, k) {
     .slice(0, k)
     .map(([value]) => value)
 };
+
+/**
+ *  Почему Map + Sort быстрее на малых данных
+ *  1. Оптимизированные встроенные методы JS
+ *  2. Низкий константный оверхед
+ *  3. Быстродействие Map в JS
+ *  4. JS-движки оптимизируют Map + sort()
+ */
+// Bucket sort
+// var topKFrequent = function (nums, k) {
+//   const count = {};
+//   const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+//   for (const n of nums) {
+//       count[n] = (count[n] || 0) + 1;
+//   }
+//   for (const n in count) {
+//       freq[count[n]].push(parseInt(n));
+//   }
+
+//   const res = [];
+//   for (let i = freq.length - 1; i > 0; i--) {
+//       for (const n of freq[i]) {
+//           res.push(n);
+//           if (res.length === k) {
+//               return res;
+//           }
+//       }
+//   }
+// };
---
+++ example/0.ArraysHash/0347.js
@@ -0,0 +1,25 @@
+/**
+ * 347. Top K Frequent Elements
+ * https://leetcode.com/problems/top-k-frequent-elements/
+ * Difficulty: Medium
+ *
+ * Given an integer array `nums` and an integer `k`, return the `k` most
+ * frequent elements. You may return the answer in any order.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+
+};
+
+const example1 = topKFrequent([1, 1, 1, 2, 2, 3], 2); // [1,2]
+const example2 = topKFrequent([1], 1); // [1]
+const example3 = topKFrequent([3, 3, 5, 5, 5, 6], 2); // [3, 5]
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
