6b35a337e69edd7bee92ba422c29c219df42fefb
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-04 03:42:37 +0300

    Create 0392.js

--- example/0392.js
+++ example/0392.js
@@ -15,26 +15,17 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
-  if (s.length > t.length) return false;
-  let count = 0;
-  for (let i = 0; i < t.length; i++) {
-    if (s[count] === t[i]) {
-      count++;
-    }
-  }
-  return count === s.length;
+var isSubsequence = function(sub, t) {
+
 };
 
-// var isSubsequence = function(s, t) {
-//   let p1 = 0;
-//   let p2 = 0;
-//   while (p1 < s.length && p2 < t.length) {
-//       if (s[p1] === t[p2]) {
-//           p1 += 1
-//       }
-//       p2 += 1
-//   }
+const example1 = isSubsequence('abc', 'ahbgdc'); // true
+const example2 = isSubsequence('axc', 'ahbgdc'); // false
+const example3 = isSubsequence('abc', 'ab1c'); // true
+const example4 = isSubsequence('abc', 'abb1abc'); // true
 
-//   return p1 === s.length
-// };
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+  
