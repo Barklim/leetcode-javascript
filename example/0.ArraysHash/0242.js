b8fe79d4ffbf8946008a69d3c189aac7c8539328
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-04 10:10:35 +0300

    Create 0242.js

---
+++ example/0.ArraysHash/0242.js
@@ -0,0 +1,26 @@
+/**
+ * 242. Valid Anagram
+ * https://leetcode.com/problems/valid-anagram/
+ * Difficulty: Easy
+ *
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+}
+
+const example1 = isAnagram('anagram', "nagaram"); // true
+const example2 = isAnagram("rat", 'car'); // false
+const example3 = isAnagram('racecar', 'carrace'); // true
+const example4 = isAnagram('jar', 'jam'); // false
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
