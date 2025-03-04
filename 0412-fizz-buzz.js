0a431191d5a2ea8102c7ab0117a2533269a6668f
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-04 09:31:15 +0300

    Create 0412.js

--- 0412-fizz-buzz.js
+++ 0412-fizz-buzz.js
@@ -20,3 +20,19 @@ var fizzBuzz = function(n) {
       ? 'FizzBuzz' : i % 3 === 0 ? 'Fizz' : i % 5 === 0 ? 'Buzz' : String(i)
   );
 };
+
+// var fizzBuzz = function(n) {
+//   const result = []
+//   for (let i = 1; i <= n; i++) {
+//       if (i % 3 === 0 && i % 5 === 0) {
+//           result.push('FizzBuzz')
+//       } else if(i % 3 === 0) {
+//           result.push('Fizz')
+//       } else if(i % 5 === 0) {
+//           result.push('Buzz')
+//       } else {
+//           result.push(i+'')
+//       }
+//   }
+//   return result;
+// };
---
+++ example/20.0ther/0412.js
@@ -0,0 +1,27 @@
+/**
+ * 412. Fizz Buzz
+ * https://leetcode.com/problems/fizz-buzz/
+ * Difficulty: Easy
+ *
+ * Given an integer n, return a string array answer (1-indexed) where:
+ * - answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * - answer[i] == "Fizz" if i is divisible by 3.
+ * - answer[i] == "Buzz" if i is divisible by 5.
+ * - answer[i] == i (as a string) if none of the above conditions are true.
+ */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+
+};
+
+const example1 = fizzBuzz(3); // ["1","2","Fizz"]
+const example2 = fizzBuzz(5); // ["1","2","Fizz","4","Buzz"]
+const example3 = fizzBuzz(15); // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
