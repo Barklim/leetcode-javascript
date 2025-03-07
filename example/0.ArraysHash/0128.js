9cfd4edd08892f1ba3c5626d8862fade5e6ca206
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-08 00:40:42 +0300

    Update 0128.js

--- example/0.ArraysHash/0128.js
+++ example/0.ArraysHash/0128.js
@@ -14,23 +14,7 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    const set = new Set(nums);
-    let result = 0;
-  
-    for (const num of set) {
-      if (!set.has(num - 1)) {
-        let count = 1;
-        let n = num;
-  
-        while (set.has(n + 1)) {
-          n++;
-          count++;
-        }
-        result = Math.max(result, count);
-      }
-    }
-  
-    return result;
+
 };
   
 
