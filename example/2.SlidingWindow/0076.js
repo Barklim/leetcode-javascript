975e1e81162c9aace68ee602cfb43eda2fcf827f
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-11 11:51:21 +0300

    Create 0076.js

---
+++ example/2.SlidingWindow/0076.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+ 
+};
+  
+
+const example1 = minWindow("ADOBECODEBANC", "ABC"); // "BANC"
+const example2 = minWindow("a", "a"); // "a"
+const example3 = minWindow("a", "aa"); // ""
+const example4 = minWindow("OUZODYXAZV", "XYZ"); // "YXAZ"
+const example5 = minWindow("xyz", "xyz"); // "xyz"
+const example6 = minWindow("x", "xy"); // ""
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+console.log(example5);
+console.log(example6);
