19fec7c06daa6c29a5324a6a5e8e69d13772a43f
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-11 11:59:12 +0300

    Create 0239.js

--- 0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum.js
@@ -34,3 +34,27 @@ var maxSlidingWindow = function(nums, k) {
 
   return result;
 };
+
+// var maxSlidingWindow = function(nums, k) {
+//   let res = [];
+//   let deque = [];
+
+//   for (let idx = 0; idx < nums.length; idx++) {
+//       let num = nums[idx];
+
+//       while (deque.length && deque[deque.length - 1] < num) {
+//           deque.pop();
+//       }
+//       deque.push(num);
+
+//       if (idx >= k && nums[idx - k] === deque[0]) {
+//           deque.shift();
+//       }
+
+//       if (idx >= k - 1) {
+//           res.push(deque[0]);
+//       }
+//   }
+
+//   return res;  
+// };
---
+++ example/2.SlidingWindow/0239.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+
+};
+  
+
+const example1 = maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3); // [3,3,5,5,6,7]
+const example2 = maxSlidingWindow([1], 1); // [1]
+const example3 = maxSlidingWindow([1,2,1,0,4,2,6], 3); // [2,2,4,4,6]
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
