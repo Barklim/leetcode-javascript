fc896ed22416269466c674a4e608439df926a4e1
Barklim <46284548+Barklim@users.noreply.github.c>
2025-03-12 14:24:00 +0300

    Create 2529.js

---
+++ example/Dayly/2529.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+
+};
+
+const example1 = maximumCount([-2,-1,-1,1,2,3]); // 3
+const example2 = maximumCount([-3,-2,-1,0,0,1,2]); // 3
+const example3 = maximumCount([5,20,66,1314]); // 4
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
